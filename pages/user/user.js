f68007983d89ac1067e7a55adc5431bc74290b5b
benhaben <405062429@qq.com>
2019-09-22 22:06:04 +0800

    我的统计数字

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,4 +1,4 @@
-import { CONST, ROUTE, ROUTE_DATA } from "../../utils/constants";
+import { CONST, PAGE_SIZE, ROUTE, ROUTE_DATA } from "../../utils/constants";
 import dao from "../../utils/dao";
 const { regeneratorRuntime } = global;
 const app = getApp();
@@ -7,16 +7,30 @@ Page({
   data: {
     url: app.getAvatar(),
     nickname: app.getNickname(),
-    total: 60,
-    send_num: 1,
-    get_num: 2,
+    total: 0,
+    send_num: 0,
+    get_num: 0,
     admin: false
   },
   async onLoad() {
     try {
-      let admin = await dao.isAdmin();
+      let adminPromise = dao.isAdmin();
+      let lotteriesPromise = dao.getAllAttendLotteries(0, 0, app.getUserId());
+      let sendsPromise = dao.getSendLotteries(0, 0, app.getUserId());
+      let winPromise = dao.getWinLotteries(0, 0, app.getUserId());
+      let admin = await adminPromise;
+      let lotteriesRes = await lotteriesPromise;
+      let sendsRes = await sendsPromise;
+      let winRes = await winPromise;
+      let total = lotteriesRes.data.meta.total_count;
+      let send_num = sendsRes.data.meta.total_count;
+      let get_num = winRes.data.meta.total_count;
+
       this.setData({
-        admin
+        admin,
+        total,
+        send_num,
+        get_num
       });
     } catch (e) {
       console.log(e);
