9f36f9ff3cac2414baf2e68ab0598805032e3471
benhaben <405062429@qq.com>
2019-10-22 12:23:07 +0800

    默认图片

--- pages/add_product_lottery/add_product_lottery.js
+++ pages/add_product_lottery/add_product_lottery.js
@@ -31,7 +31,7 @@ Page({
     sponsor: null,
     product_name: null,
     product_num: null,
-    url: randomProductUrl(),
+    url: "../../images/bg.png",
     open_people_num:
       CONST.PRODUCT_DEFAULT_OPEN_PEOPLE_NUM / CONST.PRODUCT_LOTTERY_PEOPLE_UNIT,
     slide_open_people_num:
